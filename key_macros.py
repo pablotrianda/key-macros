95d130d2ce09a9e59f454b62dca468def18f06df
Pablo Triandafilide <pablotrianda@gmail.com>
2020-05-22 15:51:58 -0300

    Initial commit

---
+++ key_macros.py
@@ -0,0 +1,36 @@
+import os
+import evdev
+from evdev import InputDevice, categorize, ecodes
+dev = InputDevice('/dev/input/event21')
+dev.grab()
+
+
+# cat /proc/bus/input/devices  --> view inpunt devices
+# sudo chmod a+r /dev/input/event21 --> change permissions
+
+
+
+for event in dev.read_loop():
+	key = categorize(event)
+	if isinstance(key,evdev.events.KeyEvent):
+		if key.keystate == key.key_down:
+			if key.keycode == 'KEY_C':
+				os.system('amixer -q sset Master toggle')
+			if key.keycode == 'KEY_X':
+				os.system('amixer -q sset Master 5%+')
+			if key.keycode == 'KEY_Z':
+				os.system('amixer -q sset Master 5%-')
+			if key.keycode == 'KEY_SPACE':
+                            os.system('dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause >> /dev/null')
+			if key.keycode == 'KEY_N':
+	 			os.system('dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next >> /dev/null')
+			if key.keycode == 'KEY_B':
+				os.system('dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous >> /dev/null')
+			if key.keycode == 'KEY_A':
+				os.system('wmctrl -a Terminal ')
+			if key.keycode == 'KEY_S':
+				os.system('wmctrl -a Spotify Premium')
+
+
+	 				
+
